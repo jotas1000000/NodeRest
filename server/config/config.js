8d620716e20017dcf1821bd11c9c4218953d80b9
jotas1000000@hotmail.com <jotas1000000@hotmail.com>
2021-01-25 15:48:42 -0400

    ServerRest5

--- server/config/config.js
+++ server/config/config.js
@@ -15,6 +15,8 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 }else {
+    /*SE ha guardado todas las credenciales de la base de datos en MOngoDb en una variable de entorno de heroku con >> heroku config:set MONGO_URI="mongo+srv:...." 
+    y rescatamos ese valor con process.env.MONGO_URI */
     urlDB = process.env.MONGO_URI;
 }
 
